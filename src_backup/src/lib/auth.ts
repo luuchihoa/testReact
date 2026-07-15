dcfa3c1e09e389c2909fbdf76d465f695425729e
leenguyenzz <leenguyen.nll@gmail.com>
2026-07-16 00:50:04 +0700

    Dọn dẹp thư mục backup

--- src_backup/src/lib/auth.ts
+++
@@ -1,6 +0,0 @@
-// utils/auth.ts
-export const toFakeEmail = (username: string) =>
-  `${username.toLowerCase()}@giaoly.local`;
-
-export const fromFakeEmail = (email: string) =>
-  email.replace(/@giaoly\.local$/, "");
